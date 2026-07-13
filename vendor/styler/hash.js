077c0f4fe77a85a26454c73ae1479e1f657b9e66
Ryan Walsh <10300352+rpwalsh@users.noreply.github.com>
2026-07-13 13:02:27 -0700

    Complete synchronized RiskLab site build

---
+++ vendor/styler/hash.js
@@ -0,0 +1,9 @@
+export function stableHash(value) {
+    let hash = 2166136261;
+    for (let index = 0; index < value.length; index += 1) {
+        hash ^= value.charCodeAt(index);
+        hash = Math.imul(hash, 16777619);
+    }
+    return (hash >>> 0).toString(36);
+}
+//# sourceMappingURL=hash.js.map
